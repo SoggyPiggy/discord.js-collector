618a4919447074d3352a375eeaf1238ed2765102
SoggyPiggy <soggypiggy@gmail.com>
2018-03-31 05:15:58 -0400

    Adjusted some properties and added an inherit for cards

--- src/structures/Card.js
+++ src/structures/Card.js
@@ -7,29 +7,45 @@ module.exports = class Card
 		if (typeof data.set				=== 'undefined')	throw new Error('Card missing Set');
 		if (typeof data.title			=== 'undefined')	data.title			= 'undefined';
 		if (typeof data.rarity			=== 'undefined')	data.rarity			= 'undefined';
-		if (typeof data.xp				=== 'undefined')	data.xp				= 100;
-		if (typeof data.chance			=== 'undefined')	data.chance			= 100;
 		if (typeof data.source			=== 'undefined')	data.source			= null;
 		if (typeof data.description	=== 'undefined')	data.description	= null;
-		if (typeof data.tags				=== 'undefined')	data.tags			= data.set.tags;
-		if (typeof data.author			=== 'undefined')	data.author			= data.set.author;
+		if (typeof data.tags				=== 'undefined')	data.tags			= [];
+		if (typeof data.author			=== 'undefined')	data.author			= null;
+		if (typeof data.xp				=== 'undefined')	data.xp				= 100;
+		if (typeof data.chance			=== 'undefined')	data.chance			= 100;
+		if (typeof daat.visibility		=== 'undefined')	data.visibility	= 1;
 		if (typeof data.image			=== 'undefined')	data.image			= null;
 		if (typeof data.fullart			=== 'undefined')	data.fullart		= false;
 		if (typeof data.guarded			=== 'undefined')	data.guarded		= false;
 		if (typeof data.untradable		=== 'undefined')	data.untradable	= false;
+		if (typeof data.omit				=== 'undefined')	data.omit			= false;
 		
 		this.id				= data.id;
 		this.set				= data.set;
 		this.title			= data.title;
-		this.xp				= data.xp;
 		this.rarity			= data.rarity;
-		this.chance			= data.chance;
 		this.source			= data.source;
 		this.description	= data.description;
 		this.tags			= data.tags;
+		this.xp				= data.xp;
+		this.chance			= data.chance;
+		this.visibility	= data.visibility;
 		this.image			= data.image;
 		this.fullart		= data.fullart;
 		this.guarded		= data.guarded;
 		this.untradable	= data.untradable;
+		this.omit			= data.omit;
+	}
+
+	inheritProperties()
+	{
+		if (this.author === null && card.set.author !== null) this.author = card.set.author;
+		if (this.visibility < card.set.visibility) this.visibility = card.set.visibility;
+		if (card.set.guarded === true) this.guarded = true;
+		if (card.set.untradable === true) this.untradable = true;
+		for (let tag of card.set.tags)
+		{
+			if (!this.tags.includes(tag)) this.tags.push(tag);
+		}
 	}
 }
--- src/structures/Set.js
+++ src/structures/Set.js
@@ -1,4 +1,4 @@
-const Card = require('./Card');
+const ChanceMap = require('./../utils/ChanceMap');
 
 module.exports = class Set
 {
@@ -7,20 +7,28 @@ module.exports = class Set
 		if (typeof data					!== 'object')		throw new Error('Set data must be an object');
 		if (typeof data.id				=== 'undefined')	throw new Error('Set missing ID');
 		if (typeof data.title			=== 'undefined')	data.title			= 'undefined';
-		if (typeof data.chance			=== 'undefined')	data.chance			= 100;
 		if (typeof data.author			=== 'undefined')	data.author			= null;
 		if (typeof data.description	=== 'undefined')	data.description	= null;
-		if (typeof data.author			=== 'undefined')	data.author			= null;
-		if (typeof data.purchasable	=== 'undefined')	data.purchasable	= null;
 		if (typeof data.tags				=== 'undefined')	data.tags			= [];
+		if (typeof data.chance			=== 'undefined')	data.chance			= 100;
+		if (typeof data.visibility		=== 'undefined')	data.visibility	= 1;
+		if (typeof data.purchasable	=== 'undefined')	data.purchasable	= true;
+		if (typeof data.obtainable		=== 'undefined')	data.obtainable	= true;
+		if (typeof data.guarded			=== 'undefined')	data.guarded		= false;
+		if (typeof data.untradable		=== 'undefined')	data.untradable	= false;
+		if (typeof data.omit				=== 'undefined')	data.omit			= false;
 
 		this.id				= data.id;
 		this.title			= data.title;
 		this.chance			= data.chance;
 		this.author			= data.author;
 		this.description	= data.description;
-		this.author			= data.author;
-		this.purchasable	= data.purchasable;
 		this.tags			= data.tags;
+		this.purchasable	= data.purchasable;
+		this.obtainable	= data.obtainable;
+		this.guarded		= data.guarded;
+		this.untradable	= data.untradable;
+		this.omit			= data.omit;
+		this.cards			= new ChanceMap();
 	}
 }
