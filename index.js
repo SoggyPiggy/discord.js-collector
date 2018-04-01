bcdd2c349b328e089cdf8b6df899b927f83f31c5
SoggyPiggy <soggypiggy@gmail.com>
2018-04-01 18:33:32 -0400

    Added a more open level of visibility

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ module.exports =
 	Card: require('./src/structures/Card'),
 	Set: require('./src/structures/Set'),
 
-	visibility: {NORMAL: 1, HIDDEN: 2, INVISIBLE: 3},
+	visibility: {OPEN: 0, NORMAL: 1, HIDDEN: 2, INVISIBLE: 3},
 	version: require('./package').version,
 }
