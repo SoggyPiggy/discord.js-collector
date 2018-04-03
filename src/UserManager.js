6cbb84ccafba534616d4e56c4f2e3365baa7926c
SoggyPiggy <soggypiggy@gmail.com>
2018-04-03 04:54:25 -0400

    the db is now registered to the useramager

--- src/UserManager.js
+++ src/UserManager.js
@@ -9,14 +9,14 @@ module.exports = class UserManager extends Map
 		super();
 		this.collector = collector;
 
-		let db = diskdb.connect(path.join(collector.options.database), ['users']);
-		let userDB = diskdb.connect(path.join(collector.options.database, 'users'))
-		let users = db.users.find();
+		this.db = diskdb.connect(path.join(collector.options.database), ['users']);
+		this.userDB = diskdb.connect(path.join(collector.options.database, 'users'))
+		let users = this.db.users.find();
 		for (let user of users)
 		{
 			let id = String(user.id);
-			userDB.loadCollections([id]);
-			let userData = userDB[id].find();
+			this.userDB.loadCollections([id]);
+			let userData = this.userDB[id].find();
 			if (typeof userData.id === 'undefined')
 			{
 				this.collector.emit('warn', `Attempting to register a User without ID: ${user}; skipping`);
