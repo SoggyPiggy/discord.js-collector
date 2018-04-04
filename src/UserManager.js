103d8eff612a73b9e95bdabfb1f55d11c1bdeed2
SoggyPiggy <soggypiggy@gmail.com>
2018-04-04 00:43:50 -0400

    Added proper functionality to the UserManager

--- src/UserManager.js
+++ src/UserManager.js
@@ -14,7 +14,7 @@ module.exports = class UserManager extends Map
 		let users = this.db.users.find();
 		for (let p in users)
 		{
-			let id = String(users[p].id);
+			let id = this.parseID(users[p]);
 			this.userDB.loadCollections([id]);
 			let data = this.userDB[id].find();
 			if (data.length <= 0)
@@ -30,5 +30,45 @@ module.exports = class UserManager extends Map
 			let user = new User(collector, data);
 			this.set(user.id, user);
 		}
+	}
+
+	get(member, create = true)
+	{
+		let id = this.parseID(member);
+		let user = super.get(id);
+		if (typeof user === 'undefined')
+		{
+			if (!create) return false;
+			user = new User(this.collector, {id: id});
+			this.set(user.id, user);
+		}
+		if (typeof member.username !== 'undefined') user.username = `${member.username}#${member.discriminator}`;
+		return user
+	}
+
+	has(member)
+	{
+		let id = this.parseID(member);
+		return super.has(id);
+	}
+
+	save(member)
+	{
+		let id = this.parseID(member);
+		let user = this.get(id, false);
+		if (user)
+		{
+			let updated = this.db.users.update({id, id}, {id, id}, {upsert: true});
+			if (updated.inserted > 0) this.userDB.loadCollections([id]);
+			this.userDB[id].update({id: id}, user.compress(), {upsert: true});
+			this.collector.emit('debug', `User ${id} saved.`);
+			this.collector.emit('userSaved', user)
+		}
+	}
+
+	parseID(member)
+	{
+		if (typeof member === 'string') return member;
+		else if (typeof member.id !== 'undefined') return String(member.id);
 	}
 }
