9bd35f0cf5a1c9a91454a549cea2b3d9a8225c17
SoggyPiggy <soggypiggy@gmail.com>
2018-04-07 08:18:35 -0400

    Added the set information command

--- src/commands/set/set.js
+++ src/commands/set/set.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 const Commando = require('discord.js-commando');
 
 module.exports = class _Command extends Commando.Command
@@ -7,13 +8,66 @@ module.exports = class _Command extends Commando.Command
 		super(Client,
 			{
 				name: 'set-info',
+				aliases: ['set', 's'],
 				group: 'collector_sets',
 				memberName: 'set-info',
-				description: 'set-info'
+				description: 'View information about a set.',
+				// details: 'Bah',
+				args: [Collector.utils.args.setID, Collector.utils.args.page]
 			});
 		this.collector = Collector;
 	}
 
 	async run(message, args)
-	{ }
+	{
+		let setID = this.collector.utils.formatSetID(args.setID);
+		let set = this.collector.registry.sets.get(setID);
+		if (typeof set === 'undefined')
+		{
+			message.reply(`Unable to find Set: \`${setID}\``);
+			return
+		}
+		if (set.visibilty > 2)
+		{
+			message.reply(`Unable to find Set: \`${setID}\``);
+			return
+		}
+		let user = this.collector.users.get(message.author);
+		let cards = Array.from(set.cards.values());
+		if (set.visibilty > 1)
+		{
+			cards = set.ownedCards(user);
+			if (cards.length <= 0)
+			{
+				message.reply(`Unable to find Set: \`${setID}\``);
+				return;
+			}
+		}
+
+		let description = `**Set:** ${set.title} \`${set.id}\``;
+		cards = this.collector.utils.pagify(args.page, cards);
+		description += `\n**Cards:**\n` + this.collector.utils.cardList(cards.results, user);
+		description += `\n`;
+		if (set.author) description += `\n**Author:** <@${set.author}>`
+		if (set.tags > 0)
+		{
+			description += '\n**Tags:** '
+			for (let tag of set.tags)
+			{
+				description += `\`${tag}\`, `;
+			}
+			description = description.replace(/, $/g, '');
+		}
+		if (set.description) description += `\n${set.description}`;
+
+		let footer = '';
+		if (set.series.collectable) footer += 'Collectable ';
+		if (set.series.packable) footer += 'Purchasable ';
+		footer = footer.replace(/ $/g, '');		
+
+		let embed = new Discord.MessageEmbed();
+		embed.setDescription(description);
+		embed.setFooter(footer);
+		message.channel.send(embed);
+	}
 }
