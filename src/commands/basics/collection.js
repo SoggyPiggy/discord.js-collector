0ee39368b122ed5a3d91e71445ae52dedfb70e94
SoggyPiggy <soggypiggy@gmail.com>
2018-04-09 11:08:37 -0400

    added to the ocllection command

--- src/commands/basics/collection.js
+++ src/commands/basics/collection.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 const Commando = require('discord.js-commando');
 
 module.exports = class _Command extends Commando.Command
@@ -9,11 +10,60 @@ module.exports = class _Command extends Commando.Command
 				name: 'collection',
 				group: 'collector_basic',
 				memberName: 'collection',
-				description: 'collection'
+				description: 'View your collected cards.',
+				args:[Collector.utils.args.pageUser, Collector.utils.args.user]
 			});
 		this.collector = Collector;
 	}
 
 	async run(message, args)
-	{ }
+	{
+		let user;
+		let author = this.collector.users.get(message.author, false);
+		if (args.page instanceof Discord.User)
+		{
+			user = this.collector.users.get(args.page, false);
+			if (!user)
+			{
+				message.reply(`<@${args.page.id}> does not own any cards.`);
+				return;
+			}
+			args.page = 1;
+		}
+		else if (args.member instanceof Discord.User)
+		{
+			user = this.collector.users.get(args.member, false);
+			if (!user)
+			{
+				message.reply(`<@${args.member.id}> does not own any cards.`);
+				return;
+			}
+		}
+		else
+		{
+			user = author;
+			if (!user)
+			{
+				message.reply('You have yet to collect and do not have Cards.\nUse the help command to learn more.');
+				return;
+			}
+		}
+		let cards = Array.from(user.cards.keys());
+		if (cards.length <= 0)
+		{
+			if (user == author) message.reply('You do not own any cards.');
+			else message.reply(`<@${user.id}> does not own any cards.`);
+			return;
+		}
+		cards.sort();
+		cards = this.collector.utils.pagify(args.page, cards);
+
+		let description = `<@${user.id}>'s Collection.`;
+		description += `\n~~\`----------------\`~~\` (Page ${cards.page} of ${cards.max}) \`~~\`----------------\`~~\n`;
+		description += this.collector.utils.cardList(cards.results, user, {collected: false});
+
+		let embed = new Discord.MessageEmbed();
+		embed.setDescription(description);
+		message.channel.send(embed);
+	}
 }
