0ec64c607d64571f483f9f8e03b99adf66b3511c
SoggyPiggy <soggypiggy@gmail.com>
2018-04-17 02:14:15 -0400

    Added functionality to the collect command

--- src/commands/basics/collect.js
+++ src/commands/basics/collect.js
@@ -4,16 +4,39 @@ module.exports = class _Command extends Commando.Command
 {
 	constructor(Client, Collector)
 	{
+		let time = '';
+		if (Collector.options.collectMaxCooldown != Collector.options.collectMinCooldown) time = `${Collector.utils.formatTime(Collector.options.collectMinCooldown)} to ${Collector.utils.formatTime(Collector.options.collectMaxCooldown)}` 
+		else time = `${Collector.utils.formatTime(Collector.options.collectMinCooldown)}`
 		super(Client,
 			{
 				name: 'collect',
 				group: 'collector_basic',
 				memberName: 'collect',
-				description: 'collect'
+				description: `Collect a new card every ${time}.`
 			});
 		this.collector = Collector;
 	}
 
 	async run(message, args)
-	{ }
+	{
+		let user = this.collector.users.get(message.author);
+		let cooldown = user.cooldowns.get('collect');
+		if (!cooldown.ready)
+		{
+			message.reply('You can not collect yet.');
+			this.collector.emit('collectFailed', user, mesasge);
+			return;
+		}
+		let reply = await message.channel.send(`<@${user.id}> Collecting Card...`);
+		cooldown.trigger();
+		let series = this.collector.registry.collectable.random();
+		let set = series.sets.random();
+		let card = set.cards.random();
+		let isNew = !user.cards.has(card);
+		user.cards.add(card);
+		user.giveXP(card.xp);
+		user.save();
+		reply.edit(`<@${user.id}> Collected \`${card.id}\` **${card.title}** *${card.rarity}*`);
+		this.collector.emit('collect', card, user, message);
+	}
 }
