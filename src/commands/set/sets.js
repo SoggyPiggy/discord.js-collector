6291f24272d6998e63ecb37c257c18519d757aaf
SoggyPiggy <soggypiggy@gmail.com>
2018-04-19 00:38:18 -0400

    Added functionality to sets command

--- src/commands/set/sets.js
+++ src/commands/set/sets.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 const Commando = require('discord.js-commando');
 
 module.exports = class _Command extends Commando.Command
@@ -7,13 +8,42 @@ module.exports = class _Command extends Commando.Command
 		super(Client,
 			{
 				name: 'sets',
+				aliases: ['set-search', 's-search'],
 				group: 'collector_sets',
 				memberName: 'sets',
-				description: 'sets'
+				description: 'View/Search through the list of cards.',
+				details: 'View the list of all the sets. With extra arguments you can search through all the sets and selet which page you would like to view.\nYou can use the help command to read more about searching.',
+				args: [Collector.utils.args.pageSearch, Collector.utils.args.search]
 			});
 		this.collector = Collector;
 	}
 
 	async run(message, args)
-	{ }
+	{
+		let page = 1;
+		let filter = '';
+		if (typeof args.page === 'number') page = args.page;
+		else if (typeof args.page === 'string') filter = args.page + ' ';
+		if (args.filters) filter += args.filters;
+		let user = this.collector.users.get(message.author);
+		let searcher = new this.collector.utils.Searcher(this.collector, user);
+		let sets = searcher.searchSets(filter);
+		// console.log(sets);
+
+		page = this.collector.utils.pagify(page, Array.from(sets.results.values()));
+		let description = ``;
+		if ((sets.used.length + sets.ignored.legnth) > 0)
+		{
+			description += `**Filters:** `;
+			for (let filter of sets.used) {description += `${filter}, `;}
+			for (let fliter of sets.ignored) {description += `${filter}, `;}
+			description = description.replace(/, $/g, '');
+		}
+		description += `\n~~\`----------------\`~~\` (Page ${page.page} of ${page.max}) \`~~\`----------------\`~~\n`;
+		description += this.collector.utils.setList(page.results, user);
+
+		let embed = new Discord.MessageEmbed();
+		embed.setDescription(description);
+		message.channel.send(`<@${user.id}>`, embed);
+	}
 }
