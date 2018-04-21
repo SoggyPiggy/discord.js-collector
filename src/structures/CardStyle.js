4a7cc16019395dea33bcbfa17f1e3eada39e37c3
SoggyPiggy <soggypiggy@gmail.com>
2018-04-21 16:19:04 -0400

    Added more functionality to the card style class

--- src/structures/CardStyle.js
+++ src/structures/CardStyle.js
@@ -22,16 +22,59 @@ module.exports = class CardStyle
 		if (typeof options.width === 'undefined') throw new Error('Card Style missing Width.');
 		if (typeof options.height === 'undefined') throw new Error('Card Style missing Height.');
 		if (typeof options.description === 'undefined') options.description = null;
+		if (typeof options.defaults !== 'object') options.defaults = {};
+		if (typeof options.defaults.x === 'undefined') options.defaults.x = 0;
+		if (typeof options.defaults.y === 'undefined') options.defaults.y = 0;
+		if (typeof options.defaults.font === 'undefined') options.defaults.font = 'Arial';
+		if (typeof options.defaults.size === 'undefined') options.defaults.size = 12;
+		if (typeof options.defaults.color === 'undefined') options.defaults.color = '#000000';
+		this.id = options.id;
+		this.title = options.title;
+		this.width = options.width;
+		this.height = options.height;
+		this.options = options;
+		this.layers = [];
 	}
 
-	addTextLayer(data = {})
+	addLayer(options = {})
 	{
-		if (typeof data !== 'object') data = {};
-		if (typeof data.id === 'undefined') data.id = null;
-		if (typeof data.text === 'undefined') data.text = 'No Text';
-		if (typeof data.font === 'undefined') data.font = 'Arial';
-		if (typeof data.size === 'undefined') data.size = 12;
-		if (typeof data.color === 'undefined') data.color = '#000000';
-		if (typeof data.width === 'undefined') data.width = Infinity;
+		if (typeof options !== 'object') options = {};
+		if (typeof options.x === 'undefined') options.x = this.options.defaults.x;
+		if (typeof options.y === 'undefined') options.y = this.options.defaults.y;
+		if (typeof options.color === 'undefined') options.color = this.options.defaults.color;
+		if (typeof options.width === 'undefined') options.width = null;
+		if (typeof options.height === 'undefined') options.height = null;
+		if (typeof options.validate === 'undefined') options.validate = null;
+		if (typeof options.fallback === 'undefined') options.fallback = null;
+		if (!options.content) return;
+		this.layers.push(options);
+	}
+	
+	addTextLayer(text, options = {})
+	{
+		if (typeof options !== 'object') options = {};
+		if (typeof options.font === 'undefined') options.font = this.options.defaults.font;
+		if (typeof options.size === 'undefined') options.size = this.options.defaults.size;
+		options.content = text;
+		options.type = 'text';
+		this.addLayer(options);
+	}
+
+	addImageLayer(url, options = {})
+	{
+		if (typeof options !== 'object') options = {};
+		options.content = url;
+		options.type = 'image';
+		this.addLayer(options);
+	}
+
+	addRectLayer(options = {})
+	{
+		if (typeof options !== 'object') options = {};
+		if (typeof options.width === 'undefined') options.width = this.width;
+		if (typeof options.height === 'undefined') options.height = this.height;
+		options.content = true;
+		options.type = 'rect';
+		this.addLayer(options);
 	}
 }
