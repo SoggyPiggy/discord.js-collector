31f1b1554769a106bafeda6071d360fbd48a3c66
SoggyPiggy <soggypiggy@gmail.com>
2018-04-24 04:05:13 -0400

    added alpha support

--- src/structures/CardStyle.js
+++ src/structures/CardStyle.js
@@ -28,6 +28,7 @@ module.exports = class CardStyle
 		if (typeof options.defaults.font === 'undefined') options.defaults.font = 'Arial';
 		if (typeof options.defaults.size === 'undefined') options.defaults.size = 12;
 		if (typeof options.defaults.color === 'undefined') options.defaults.color = '#000000';
+		if (typeof options.defaults.alpha === 'undefined') options.defaults.alpha = 1;
 		if (typeof options.defaults.shrink === 'undefined') options.defaults.shrink = false;
 		this.id = options.id;
 		this.title = options.title;
@@ -43,6 +44,7 @@ module.exports = class CardStyle
 		if (typeof options.x === 'undefined') options.x = this.options.defaults.x;
 		if (typeof options.y === 'undefined') options.y = this.options.defaults.y;
 		if (typeof options.color === 'undefined') options.color = this.options.defaults.color;
+		if (typeof options.alpha === 'undefined') options.alpha = this.options.defaults.alpha;
 		if (typeof options.width === 'undefined') options.width = null;
 		if (typeof options.height === 'undefined') options.height = null;
 		if (typeof options.validate === 'undefined') options.validate = null;
@@ -76,5 +78,39 @@ module.exports = class CardStyle
 		options.content = true;
 		options.type = 'rect';
 		this.addLayer(options);
+	}
+
+	render(card, options = {})
+	{
+		return new Promise(async (resolve, reject) =>
+		{
+			if (typeof options !== 'object') options = {};
+			if (typeof options.new === 'undefined') options.new = false;
+			if (typeof options.user === 'undefined') options.user = null;
+
+			let data = {};
+			data.card = card;
+			data.set = card.set;
+			data.series = card.set.series;
+			data.$set = card.$set;
+			data.$series = card.$set.series;
+			data.new = options.new;
+			if (card.author)
+			{
+				data.author = {};
+				data.author.user = this.collector.users.get(card.author, false);
+				data.author.member = await this.collector.Commando.users.fetch(card.author);
+			}
+			try
+			{
+				let renderer = await this.collector.options.renderer;
+				let buffer = await renderer(this, data);
+				resolve(buffer);
+			}
+			catch(error)
+			{
+				reject(error);
+			}
+		})
 	}
 }
