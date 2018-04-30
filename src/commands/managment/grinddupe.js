4af3def34670a62f05c2aaff9c3b07125cc8d643
SoggyPiggy <soggypiggy@gmail.com>
2018-04-30 10:21:17 -0400

    Added functionalities to the grind commands

--- src/commands/managment/grinddupe.js
+++ src/commands/managment/grinddupe.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 const Commando = require('discord.js-commando');
 
 module.exports = class _Command extends Commando.Command
@@ -7,13 +8,62 @@ module.exports = class _Command extends Commando.Command
 		super(Client,
 			{
 				name: 'grind-dupe',
+				aliases: ['grind-d', 'g-d'],
 				group: 'collector_managment',
 				memberName: 'grind-dupe',
-				description: 'grind-dupe'
+				description: 'Grind the matching Cards duplicates',
+				details: 'Use filters and searching to select the cards you want to grind. If no search filters are given the filter \'-guarded\' is used.\nTo quick confirm you can just put a \'y\' at the end',
+				args: [Collector.utils.args.search]
 			});
 		this.collector = Collector;
 	}
 
 	async run(message, args)
-	{ }
+	{
+		if (args.filters === false) args.filters = '';
+		args.filters = args.filters.split(' ');
+		let confirmation = null;
+		if (args.filters.length > 0) confirmation = this.collector.utils.quickConfirm(args.filters);
+		let quickConfirm = confirmation;
+		args.filters = args.filters.join(' ');
+		if (args.filters === '') args.filters = '-guarded';
+		let user = this.collector.users.get(message.author, false);
+		if (!user) return message.reply('You do not have any cards.');
+		let handler = new this.collector.utils.CardHandler(this.collector, {user: user, items: user.cards.keys()});
+		handler.processItems();
+		handler.addFilter('own');
+		handler.sort();
+		handler.applyFilters(args.filters, [{keys: ['rarity'], threshold: 0}]);
+		let cards = handler.cards;
+		let grindInfo = new Discord.MessageEmbed();
+		grindInfo.setTitle('Grind Details')
+		grindInfo.setDescription(handler.grindList('dupe'));
+		message.channel.send(`<@${user.id}>`, grindInfo);
+		if (!confirmation)
+		{
+			let arg = {key: 'confirmation', labal: 'Confirmation', type: 'boolean'};
+			arg.prompt = `Confirm grind\n(Y)es or (N)o`;
+			let argumentcollector = new Commando.ArgumentCollector(this.client, [this.collector.utils.args.search, arg]);
+			let awaited = await argumentcollector.obtain(message, [args.filters]);
+			if (awaited.cancelled) confirmation = false;
+			else confirmation = awaited.values.confirmation;
+		}
+		if (!confirmation) return message.reply('Grind Canceled');
+		let groundCount = 0;
+		let groundUnique = 0;
+		let groundCards = [];
+		for (let [key, card] of cards)
+		{
+			let count = user.cards.get(card);
+			let ground = user.cards.remove(card, count - 1);
+			if (!ground) continue;
+			user.giveXP((card.xp * this.collector.options.grindPercent) * ground);
+			groundUnique++;
+			groundCount += ground;
+			groundCards.push({id: card.id, card: card, count: ground});
+		}
+		user.save();
+		message.reply('Grind Completed')
+		this.collector.emit('grind', groundCards, groundCount, groundUnique);
+	}
 }
