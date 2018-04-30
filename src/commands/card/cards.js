a670898ee9b82933b39921cbf09014d743f7ffea
SoggyPiggy <soggypiggy@gmail.com>
2018-04-30 16:55:17 -0400

    Converted cards and sets command to the new Handlers

--- src/commands/card/cards.js
+++ src/commands/card/cards.js
@@ -26,22 +26,11 @@ module.exports = class _Command extends Commando.Command
 		else if (typeof args.page === 'string') filter = args.page + ' ';
 		if (args.filters) filter += args.filters;
 		let user = this.collector.users.get(message.author);
-		let searcher = new this.collector.utils.Searcher(this.collector);
-		let search = searcher.searchCards(filter, {user: user});
-		let cards = Array.from(search.results.values());
-		
-		page = this.collector.utils.pagify(page, cards);
-		let description = ``;
-		if ((search.used.length + search.ignored.length) > 0)
-		{
-			description += `**Filters:** `;
-			for (let filter of search.used) { description += `${filter}, `; }
-			for (let filter of search.ignored) { description += `~~${filter}~~, `; }
-			description = description.replace(/, $/g, '');
-		}
-		description += `\n~~\`----------------\`~~\` (Page ${page.page} of ${page.max}) \`~~\`----------------\`~~\n`;
-		description += this.collector.utils.cardList(page.results, {user: user});
-
+		let handler = new this.collector.utils.CardHandler(this.collector, {user: user});
+		handler.checkVisibility();
+		handler.applyFilters(filter);
+		handler.sort();
+		let description = handler.list(page);
 		let embed = new Discord.MessageEmbed();
 		embed.setDescription(description);
 		message.channel.send(`<@${user.id}>`, embed);
--- src/commands/set/sets.js
+++ src/commands/set/sets.js
@@ -26,22 +26,10 @@ module.exports = class _Command extends Commando.Command
 		else if (typeof args.page === 'string') filter = args.page + ' ';
 		if (args.filters) filter += args.filters;
 		let user = this.collector.users.get(message.author);
-		let searcher = new this.collector.utils.Searcher(this.collector);
-		let search = searcher.searchSets(filter, {user: user});
-		let sets = Array.from(search.results.values());
-
-		page = this.collector.utils.pagify(page, sets);
-		let description = ``;
-		if ((search.used.length + search.ignored.length) > 0)
-		{
-			description += `**Filters:** `;
-			for (let filter of search.used) { description += `${filter}, `; }
-			for (let filter of search.ignored) { description += `~~${filter}~~, `; }
-			description = description.replace(/, $/g, '');
-		}
-		description += `\n~~\`----------------\`~~\` (Page ${page.page} of ${page.max}) \`~~\`----------------\`~~\n`;
-		description += this.collector.utils.setList(page.results, user);
-
+		let handler = new this.collector.utils.utils.SetHandler(this.collector, {user: user});
+		handler.applyFilters(filter);
+		handler.sort();
+		let description = handler.list(page);
 		let embed = new Discord.MessageEmbed();
 		embed.setDescription(description);
 		message.channel.send(`<@${user.id}>`, embed);
