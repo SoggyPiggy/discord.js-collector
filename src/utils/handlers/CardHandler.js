849ddb3f0f3d143825a7218ba2be106c6562cdce
SoggyPiggy <soggypiggy@gmail.com>
2018-04-30 09:34:54 -0400

    Moved so empty rarities would not be shown

--- src/utils/handlers/CardHandler.js
+++ src/utils/handlers/CardHandler.js
@@ -215,12 +215,12 @@ module.exports = class CardHandler extends Handler
 		for (let [key, item] of this.items)
 		{
 			let grind = count;
-			if (typeof rarities[item.rarity] === 'undefined') rarities[item.rarity] = 0;
 			let owned = this.user.cards.get(item);
 			if (count === 'all') grind = owned;
 			else if (count === 'dupe') grind = owned - 1;
 			else if (count > owned) grind = owned;
 			if (!grind) continue;
+			if (typeof rarities[item.rarity] === 'undefined') rarities[item.rarity] = 0;
 			total += grind;
 			unique++;
 			rarities[item.rarity] += grind;
