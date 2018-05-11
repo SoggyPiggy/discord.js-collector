2e9a7bd625827aaca605c344cf1cc61fcf0ed822
SoggyPiggy <soggypiggy@gmail.com>
2018-05-11 09:14:56 -0400

    added functionality to offers command

--- src/commands/trading/offers.js
+++ src/commands/trading/offers.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 const Commando = require('discord.js-commando');
 
 module.exports = class _Command extends Commando.Command
@@ -9,11 +10,44 @@ module.exports = class _Command extends Commando.Command
 				name: 'offers',
 				group: 'collector_trading',
 				memberName: 'offers',
-				description: 'offers'
+				description: 'offers',
+				args:
+				[
+					{
+						key: 'page',
+						label: 'Page Number / Filter',
+						prompt: 'Which page would you like to view? or what filters would you like to apply?',
+						type: 'integer|string',
+						default: 1
+					},
+					{
+						key: 'filters',
+						label: 'Filter(s)',
+						prompt: 'What filters would you like to apply?',
+						type: 'string',
+						default: false
+					}
+				]
 			});
 		this.collector = Collector;
 	}
 
 	async run(message, args)
-	{ }
+	{
+		let user = this.collector.users.get(message.author, false);
+		if (!user) return message.reply('You do not have any trades.');
+		if (user.trades.size <= 0) return message.reply('You do not have any trades.');
+		let page = 1;
+		let filter = '';
+		if (typeof args.page === 'number') page = args.page;
+		else if (typeof args.page === 'string') filter = args.page + ' ';
+		if (args.filters) filter += args.filters;
+		let handler = new this.collector.utils.OfferHandler(this.collector, {user});
+		handler.applyFilters(filter);
+		handler.sort();
+		let description = handler.list(page);
+		let embed = new Discord.MessageEmbed();
+		embed.setDescription(description);
+		message.channel.send(`${user}`, embed);
+	}
 }
