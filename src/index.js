88b2abf1f697843c781e0a185581e54acee89cff
SoggyPiggy <soggypiggy@gmail.com>
2018-05-22 11:01:25 -0400

    Added a quickset and quick card creation so there is less code.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ module.exports =
 	CardStyle: require('./structures/CardStyle'),
 	Upgrade: require('./structures/Upgrade'),
 	Util: require('./Utils'),
+	
+	quickCard: options => class extends (require('./structures/Card')) { constructor() { super(options) } },
+	quickSet: options => class extends (require('./structures/Set')) { constructor() { super(options) } },
 
 	visibility: {OPEN: 0, NORMAL: 1, HIDDEN: 2, INVISIBLE: 3},
 	version: require('./../package').version,
